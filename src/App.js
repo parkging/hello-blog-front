788ae27041426e81867af2a7f649f9940677e122
parkging <parkjun611@gmail.com>
2023-03-23 04:24:48 +0900

    todo list

--- src/App.js
+++ src/App.js
@@ -1,48 +1,28 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
-const Hello = () => {
-  const byFnc = () => console.log("destroyed");
-  const hiFnc = () => {
-    console.log("created");
-    return byFnc;
-  };
-  useEffect(hiFnc, []);
-  return <h1>Hello</h1>;
-};
+import { useState } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("all time render");
-
-  useEffect(() => console.log("i run only once"), []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5)
-      console.log("search", { keyword });
-  }, [keyword]);
-  const onClickShowBtn = () => setShowing((prevValue) => !prevValue);
+  const [todo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
   return (
     <div>
-      {showing ? (
-        <div>
-          <Hello />
-          <input
-            value={keyword}
-            type="text"
-            placeholder="Search here..."
-            onChange={onChange}
-          />
-          <h1> {counter} </h1>
-          <button onClick={onClick}>click me</button>
-        </div>
-      ) : null}
-      <button onClick={onClickShowBtn}> {showing ? "Hide" : "Show"} </button>
+      <form onSubmit={(event) => event.preventDefault()}>
+        <input
+          type={"text"}
+          placeholder="Write your to do..."
+          onChange={(event) => setTodo(event.target.value)}
+        />
+        <button
+          onClick={() => setTodos((currentArray) => [todo, ...currentArray])}
+        >
+          add
+        </button>
+        <hr />
+        <ul>
+          {toDos.map((mapTodo, index) => (
+            <li key={index}>{mapTodo}</li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 };
