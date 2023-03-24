d76c44e6fd38cb735fa0334584271c3ea0ec611e
parkging <parkjun611@gmail.com>
2023-03-24 13:33:22 +0900

    add basename to Router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PBLIC_URL}>
       <Switch>
         <Route path="/movie/:id">
           <Detail />
