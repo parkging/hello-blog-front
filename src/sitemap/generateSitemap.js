8fce44fccb2bf6977243a4a1d9b598b0e71afa5d
parkging <parkjun611@gmail.com>
2023-05-10 02:54:56 +0900

    sitemap 수정

--- src/sitemap/generateSitemap.js
+++ src/sitemap/generateSitemap.js
@@ -1,14 +1,17 @@
+process.env.BABEL_ENV = "development";
+process.env.NODE_ENV = "development";
+
 require("@babel/register")({
   presets: ["es2015", "react"],
 });
 
-const router = require("./SitemapRoutes").default; // 좀 전에 만든 sitemapRoutes 파일이 있는 경로입니다.
+const router = require("./SitemapRoutes").default;
 const Sitemap = require("react-router-sitemap").default;
 
 function generateSitemap() {
   return new Sitemap(router)
-    .build("https://blog.parkging.com/") // 여러분의 도메인 이름으로 변경해주세요.
-    .save("./sitemap.xml"); // sitemap.xml 파일이 생성될 위치입니다.
+    .build("https://blog.parkging.com/")
+    .save("./sitemap.xml");
 }
 
 generateSitemap();
