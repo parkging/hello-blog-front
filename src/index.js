3cd600215e28e40343c06687b22b4ff2dfd0def7
parkging <parkjun611@gmail.com>
2023-05-09 15:22:57 +0900

    페이지 확대축소 1.0 -> 0.9

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_API_ROOT;
 axios.defaults.discusRUL = process.env.REACT_APP_HOME_URL;
 axios.defaults.withCredentials = true;
+//페이지 확대 수준
+document.body.style.zoom = 0.9;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
